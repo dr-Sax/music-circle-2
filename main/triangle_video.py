4285f6df270a445ecac9c5429781d85286de6c75
dr-Sax <nicoromano2000@gmail.com>
2024-07-24 21:55:23 -0400

    tangram rotations

--- main/triangle_video.py
+++ main/triangle_video.py
@@ -51,6 +51,8 @@ class GraphicsWindow(Base):
         self.camera.setPosition([X, 0, Z]) 
         self.renderer.render(self.scene, self.camera) 
         self.clip = VideoFileClip(f'alphabet/bubbles.mp4')
+        self.phi = 0
+        self.pause_iter = 0
     
     def get_height(self, base, side):
         height = np.sqrt(side ** 2 - (base / 2) ** 2)
@@ -61,79 +63,95 @@ class GraphicsWindow(Base):
         frame_surface = pygame.surfarray.make_surface(current_frame.swapaxes(0, 1))
         return frame_surface
     
-    def get_triangle_mesh(self, vid_tstamp, base, phi, x, y):
-        frame_surface = self.get_cur_video_frame(vid_tstamp)
-        grid = Texture(frame_surface)
-        grid_material = TextureMaterial(grid)
+    def get_triangle_mesh(self, vid_tstamp, base, phi, x, y, grid_material):
         side = base / TRIANGLE_WH_P
         height = self.get_height(base, side)
         geometry = IsoscelesTriangleGeometry(base = base, height = height)
         mesh = Mesh(geometry, grid_material)  
         mesh.translate(x = x, y = y, z = 0)
         mesh.rotateZ(phi)
-        
         return mesh
     
-    def get_square_mesh(self, vid_tstamp, base, phi, x, y):
-        frame_surface = self.get_cur_video_frame(vid_tstamp)
-        grid = Texture(frame_surface)
-        grid_material = TextureMaterial(grid)
+    def get_square_mesh(self, vid_tstamp, base, phi, x, y, grid_material):
         geometry = RectangleGeometry(width = base, height = base)
         mesh = Mesh(geometry, grid_material)  
         mesh.translate(x = x, y = y, z = 0)
         mesh.rotateZ(phi)
-
         return mesh
 
-    def get_parallelogram_mesh(self, vid_tstamp, base, height, phi, x, y):
-        frame_surface = self.get_cur_video_frame(vid_tstamp)
-        grid = Texture(frame_surface)
-        grid_material = TextureMaterial(grid)
+    def get_parallelogram_mesh(self, vid_tstamp, base, height, phi, x, y, grid_material):
         geometry = ParallelogramGeometry(base = base, height = height)
         mesh = Mesh(geometry, grid_material)  
         mesh.translate(x = x, y = y, z = 0)
         mesh.rotateZ(phi)
-
         return mesh
-
-    # frame updater
-    def update(self):
-        b1 = 16
+    
+    def draw_house(self, b1, grid_material, phi):
         s1 = b1 / TRIANGLE_WH_P
         h1 = self.get_height(b1, s1)
         x1 = X0 + h1 / (2 * np.sqrt(2))
         y1 = Y0
-        TA1_mesh = self.get_triangle_mesh(self.time, b1, 3 * np.pi / 4, x1, y1)
+        theta = 3 * np.pi / 4 + phi
+
+        TA1_mesh = self.get_triangle_mesh(self.time, b1, theta, x1 + 10*np.sin(self.phi/100*2*np.pi), y1 + 10*np.sin(self.phi/100*2*np.pi), grid_material)
         x2 = X0 - h1 / (2 * np.sqrt(2))
         y2 = Y0
-        TA2_mesh = self.get_triangle_mesh(self.time, b1, 5 * np.pi / 4, x2, y2)
+        theta = 5 * np.pi / 4 + phi
+        TA2_mesh = self.get_triangle_mesh(self.time, b1, theta, x2, y2, grid_material)
 
         b2 = s1
         s2 = s1 / TRIANGLE_WH_P
         s3 = s2 / TRIANGLE_WH_P
         y3 = Y0 - h1 / (2 * np.sqrt(2)) - s3 / 2
         x3 = X0 + h1 / (2 * np.sqrt(2))
-        TB_mesh = self.get_triangle_mesh(self.time, b2, 0, x3, y3)
+        theta = 0 + phi
+        TB_mesh = self.get_triangle_mesh(self.time, b2, theta, x3, y3, grid_material)
 
         xS = X0 - s3
         yS = Y0 - h1 / (2 * np.sqrt(2)) - s3 / 2
-        S_mesh = self.get_square_mesh(self.time, s3, 0, xS, yS)
+        theta = 0 + phi
+        S_mesh = self.get_square_mesh(self.time, s3, theta, xS, yS, grid_material)
 
         b3 = s2
         h3 = self.get_height(b3, s3)
         x4 = xS + s3 / 2 + h3 / (2 * np.sqrt(2))
         y4 = Y0 - h1 / (2 * np.sqrt(2)) - h3 / (2 * np.sqrt(2))
-        TC1_mesh = self.get_triangle_mesh(self.time, b3, np.pi / 4, x4, y4)
+        theta = np.pi / 4 + phi
+        TC1_mesh = self.get_triangle_mesh(self.time, b3, theta, x4, y4, grid_material)
 
         x5 = x3 + s3 - h3 / (2 * np.sqrt(2))
         y5 = y4
-        TC2_mesh = self.get_triangle_mesh(self.time, b3, -np.pi / 4, x5, y5)
+        theta = -np.pi / 4 + phi
+        TC2_mesh = self.get_triangle_mesh(self.time, b3, theta, x5, y5, grid_material)
 
         x6 = x1 + h1 / (2 * np.sqrt(2))
         y6 = y1 + h1 / (2 * np.sqrt(2)) + b3 / 2
-        P_mesh = self.get_parallelogram_mesh(self.time, b3, h3, np.pi / 2, x6, y6)
+        theta = np.pi / 2 + phi
+        P_mesh = self.get_parallelogram_mesh(self.time, b3, h3, theta, x6, y6, grid_material)
 
         mesh_list = [TA1_mesh, TA2_mesh, S_mesh, TB_mesh, TC1_mesh, TC2_mesh, P_mesh]
+
+        return mesh_list
+
+
+    # frame updater
+    def update(self):
+        frame_surface = self.get_cur_video_frame(self.time)
+        grid = Texture(frame_surface)
+        grid_material = TextureMaterial(grid)
+
+        b1 = 4 * np.sin(self.time)  + 12
+
+        if self.phi == 100:
+            if self.pause_iter == 20:
+                self.phi = 0
+                self.pause_iter = 0
+            else:
+                self.pause_iter += 1
+        else:
+            self.phi += 1
+
+        mesh_list = self.draw_house(b1, grid_material, self.phi / 100 * 2*np.pi)
         for mesh in mesh_list:
             self.scene.add(mesh)
 
